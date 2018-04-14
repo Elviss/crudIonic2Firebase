db252f10c87c329789d6f720e53e7e2820b0b8a5
Elvis <slv_evs@yahoo.com.br>
2018-04-14 01:34:53 -0300

    propriedades da classe EditaCarroPage e de seus metodos

--- src/pages/edita-carro/edita-carro.ts
+++ src/pages/edita-carro/edita-carro.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+import {AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable} from "angularfire2/database-deprecated";
+import {Carro} from "../lista-carro/lista-carro";
+
 /**
  * Generated class for the EditaCarroPage page.
  *
@@ -14,12 +17,29 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'edita-carro.html',
 })
 export class EditaCarroPage {
+  ref: FirebaseObjectObservable<any>;
+  id: string;
+  carro: Carro;
+    
+  constructor(public af: AngularFireDatabase, public navCtrl: NavController, public navParams: NavParams) {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+      this.id = this.navParams.get('id');
+      this.ref = this.af.database.object('/carros/' + this.id);
+      
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad EditaCarroPage');
+  }
+
+  salvar(){
+    this.ref.update(this.carro).then(() => {
+      this.viewCtrl.dismiss();
+    });
+  }
+
+  fechar(){
+    this.viewCtrl.dismiss();
   }
 
 }
